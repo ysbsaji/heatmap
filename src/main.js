e55bdf64256adc8c821f2008a899be8d76a92723
ysbsaji <ysbsaji999@gmail.com>
2021-09-25 11:32:18 +0530

    project setup done

--- src/main.js
+++ src/main.js
@@ -4,12 +4,18 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import apiServices from './plugins/apiservices'
+import index from './mixins/index'
 import axios from 'axios'
 
 Vue.config.productionTip = false
 let plugins = { apiServices }
 Object.keys(plugins).forEach(key => {
   Vue.use(plugins[key])
+})
+
+let mixins = { index }
+Object.keys(mixins).forEach(key => {
+  Vue.mixin(mixins[key])
 })
 
 new Vue({
