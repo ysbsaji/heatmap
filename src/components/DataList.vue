a2b991f3aec552f0668bdb25a62174c589c34638
ysbsaji <ysbsaji999@gmail.com>
2021-09-25 11:39:28 +0530

    datalist component added

--- src/components/DataList.vue
+++ src/components/DataList.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
-    <h1>component</h1>
+    <v-data-table
+      :headers="payload.headers"
+      :items="payload.list"
+      :loading="payload.loading"
+      class="elevation-1"
+    >
+    </v-data-table>
   </div>
 </template>
 
 <script>
 export default {
-
+  props: ['payload'],
 }
 </script>
 
--- src/views/HeatMap.vue
+++ src/views/HeatMap.vue
@@ -1,12 +1,20 @@
 <template>
   <div>
-    <h1>heatmap</h1>
+    <data-list :payload="listOfPayload" class="my-3 pa-3"/>
   </div>
 </template>
 
 <script>
+import DataList from '../components/DataList.vue'
 export default {
-
+  data () {
+    return {
+      listOfPayload: []
+    }
+  },
+  components: {
+    'data-list': DataList
+  }
 }
 </script>
 
